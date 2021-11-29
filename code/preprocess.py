7fcc63415b71fe5a8db263c5e6a770690abb61af
KhosrowArian <65761790+KhosrowArian@users.noreply.github.com>
2021-11-28 22:56:25 -0600

    added caption preprocessing

--- code/preprocess.py
+++ code/preprocess.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
 import os
+import re
 
 def get_image_list(filename):
     path = os.path.join(os.getcwd(), "data/Flickr8k_text", filename)
@@ -50,15 +51,78 @@ len(diff)
 
 
 
-##### Pre-processing Captions #####
-# remove diacritics
 
-# normalize 'hamza' 't marbuta' 'alif maqsura'
+# path = "/Users/khosrow/fall2021/dl/Arabic-Image-Captioning/data/Flickr8k_text/Flickr8k.arabic.full.txt"
+# with open(path) as f:
+#     imgs = f.read().splitlines()
 
-# remove punctuation
 
-# remove non Arabic letters
+def clean_caption(caption):
+    # remove diacritics (taken from the source code)
+    diacritics = re.compile("""
+                                         ّ    | # Tashdid
+                                         َ    | # Fatha
+                                         ً    | # Tanwin Fath
+                                         ُ    | # Damma
+                                         ٌ    | # Tanwin Damm
+                                         ِ    | # Kasra
+                                         ٍ    | # Tanwin Kasr
+                                         ْ    | # Sukun
+                                         ـ     # Tatwil/Kashida
+                                     """, re.VERBOSE)
+    caption = re.sub(diacritics, '', caption)
 
-# add start & end tokens 
+    # normalize (taken from the source code)
+    caption = re.sub("[إأآاٱ]", "ا", caption)
+    caption = re.sub("ى", "ي", caption)
+    caption = re.sub("ة", "ه", caption)  # replace ta2 marboota by ha2
+    caption = re.sub("گ", "ك", caption)
+    caption = re.sub("\u0640", '', caption)  # remove tatweel
 
-# pad short captions with special token to ensure similar length captions
+    # remove punctuation
+    punctuations = set(list('''`÷×؛<>_()*&^%][ـ،/:"؟.,'{}~¦+|!”…“–ـ'''))
+    caption = ''.join([char for char in caption if char not in punctuations])
+
+    # remove non-Arabic letters
+    caption = re.sub(r'[a-zA-Z]+', '', caption)
+
+    # remove numeric words
+    caption = ' '.join([word for word in caption.split() if word.isalpha()])
+
+    # add start and end tokens
+    caption = "<start>" + ' ' + caption + ' ' + "<end>"
+
+    # pad short captions with special token to ensure similar length captions
+    # TODO
+
+    return caption
+
+
+
+
+def make_capition_dict(raw_data):
+    output = {}
+
+    for line in raw_data:
+        
+        # get the image name and caption
+        if len(line.split('\t')) < 2:
+            continue
+        image_name, caption = line.split('\t')
+
+        # clean the caption
+        caption = clean_caption(caption)
+
+        # remove the last 2 characters from the end of the image name
+        image_name = image_name[:-2]
+
+        # map the image to its caption
+        if image_name in output:
+            output[image_name].append(caption)
+        else:
+            output[image_name] = [caption]
+
+    print(len(output), output)
+    return output
+
+make_capition_dict(imgs)
