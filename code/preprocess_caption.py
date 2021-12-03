5f485c19690b606c2d75e002f1988c4efc8e3cb2
Hamzah Shah <hamzahrshah@gmail.com>
2021-12-02 19:02:06 -0500

    cleaned up files, adding calls to preprocess and model in main

--- code/preprocess_caption.py
+++ code/preprocess_caption.py
@@ -1,34 +1,9 @@
-# import tensorflow as tf
-# from tensorflow.keras.preprocessing import image
-# from tensorflow.keras.applications.vgg16 import preprocess_input
 import os
 import re
 
-
-
-##### Pre-processing Images #####
-# def process_images():
-#     pass
-
-# # root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-# # img_file = os.path.join(root, "data/Flicker8k_Dataset/667626_18933d713e.jpg")
-
-# img_file = os.path.join(os.getcwd(), "data/Flicker8k_Dataset/667626_18933d713e.jpg")
-
-# img = image.img_to_array(image.load_img(img_file, target_size=(224,224)))
-
-# # img = tf.keras.applications.vgg16.preprocess_input(img)
-
-# print(img)
-
-# print(img.shape)
-
-
-
-# path = "/Users/kanzaazhar/Desktop/cs147/Arabic-Image-Captioning/data/Flickr8k_text/Flickr8k.arabic.full.txt"
-path = "/Users/khosrow/fall2021/dl/Arabic-Image-Captioning/data/Flickr8k_text/Flickr8k.arabic.full.txt"
-with open(path) as f:
-    imgs = f.read().splitlines()
+START_TOKEN = "<start>"
+END_TOKEN = "<end>"
+SPACE = " "
 
 
 def clean_caption(caption):
@@ -67,7 +42,7 @@ def clean_caption(caption):
     caption = ' '.join([word for word in caption.split() if word.isalpha()])
 
     # add start and end tokens
-    caption = "<start>" + ' ' + caption + ' ' + "<end>"
+    caption = START_TOKEN + SPACE + caption + SPACE + END_TOKEN
 
     # pad short captions with special token to ensure similar length captions
     # TODO
@@ -76,6 +51,10 @@ def clean_caption(caption):
 
 
 def make_caption_dict(raw_data):
+    '''
+    Creates a dictionary mapping each image to a list of its captions.
+    :param raw_data: file with image names and captions
+    '''
     output = {}
 
     for line in raw_data:        
@@ -98,20 +77,22 @@ def make_caption_dict(raw_data):
 
     return output
 
-"""
-Takes in the output from make_caption_dict (which is a dictionary that maps image number/name to a list of its captions),
-and constructs a dictionary that maps each unique word in all of these captions to a word id
 
-INPUT: dictionary that maps image number/name to a list of its captions
-RETURNS: vocab dictionary which maps each unique word in the captions to an id
-"""
 def make_vocab_dict(output):
+    '''
+    Takes in the output from make_caption_dict() and constructs a dictionary 
+    that maps each unique word in all of these captions to a word id.
+
+    :param output: dictionary that maps image number/name to a list of its captions
+    :returns vocab_dict: vocab dictionary which maps each unique word in the captions to an id
+    '''
+   
     vocab_dict = {}
     word_set = set()
 
     for caption_list in output.values():
         for caption in caption_list:
-            caption = caption[7:-6]
+            caption = caption[len(START_TOKEN) + len(SPACE) : - len(END_TOKEN) - len(SPACE)]
             words_array = caption.strip().split()
             word_set.update(words_array)
 
@@ -119,23 +100,38 @@ def make_vocab_dict(output):
     return vocab_dict
 
 
-output = make_caption_dict(imgs)
-make_vocab_dict(output)
-
-def tokenize_captions(output):
-    vocab_dict = make_vocab_dict(output)
+def tokenize_captions(output, vocab):
+    '''
+    Tokenizes all captions.
+    :param output: dictionary mapping each image to its captions (the output from make_caption_dict())
+    :param vocab: vocabulary dictionary
+    :returns dictionary mapping each image to its tokenized captions
+    '''
     for image in output:
         tokenized_captions = []
         for caption in output[image]:
             tokenized_caption = []
-            caption = caption[7:-6]
+            caption = caption[len(START_TOKEN) + len(SPACE) : - len(END_TOKEN) - len(SPACE)]
 
             for word in caption.split():
-                tokenized_caption.append(vocab_dict[word])
+                tokenized_caption.append(vocab[word])
             tokenized_captions.append(tokenized_caption)
         output[image] = tokenized_captions
     return output
 
-print(tokenize_captions(output))
-                
+
+def get_data(file):
+    '''
+    Returns 1) arabic vocabulary, 2) dict mapping image to list of its tokenized captions
+    '''
+    with open(file) as f:
+        imgs = f.read().splitlines()
+    
+    caption_dict = make_caption_dict(imgs)
+    vocabulary = make_vocab_dict(caption_dict)
+    tokenized_captions = tokenize_captions(caption_dict, vocabulary)
+
+    return vocabulary, tokenized_captions
+
+                    
 
