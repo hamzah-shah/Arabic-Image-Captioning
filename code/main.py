9e6047ae059cee55d086e24f719b7bb4a5841e3a
Hamzah Shah <hamzahrshah@gmail.com>
2021-12-04 17:41:44 -0500

    train, val, test split

--- code/main.py
+++ code/main.py
@@ -1,10 +1,12 @@
 import os
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from preprocess import get_data
 from model import EndtoEnd
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_FILE = "all_data.txt"
 
 def get_image_list(filename):
     path = os.path.join(ROOT, "data/Flickr8k_text", filename)
@@ -30,9 +32,51 @@ def prep_data(file, image_list):
     data = data.drop(columns=['image'])
     # data['caption'] = data['caption'].str[::-1] // note the difference between whats printed out vs whats in the file
 
-    data.to_csv(os.path.join(ROOT, "data/Flickr8k_text", "test.txt"), sep="\t", header=False, index=False)
+    prepped_data_path = os.path.join(ROOT, "data/Flickr8k_text", "all_data.txt")
+    data.to_csv(prepped_data_path, sep="\t", header=False, index=False)
+    return prepped_data_path
 
-    # print(data)
+def split_data(img_to_captions, train_list, val_list, test_list, vocab_size, max_len=20):
+    '''
+    Prepares the data to be passed into the model.
+    :param img_to_captions: dictionary mapping each image to a list of captions
+    :param train_list: list of images in training set 
+    :param val_list: list of images in validaton set
+    :param test_list: list of images in test set
+    :param vocab_size: size of the vocabulary
+    :param max_len: the maximum length of a caption
+    :returns X_train, Y_train, X_val, Y_val, X_test, Y_test
+    '''
+    X_train, Y_train = [], []
+    X_val, Y_val = [], []
+    X_test, Y_test = [], []
+
+    def split_helper(X, Y):
+        captions = img_to_captions[image]
+        for cap in captions:
+            # loop through it -> pad its input & one-hot its output
+            for ind in range(len(cap)):
+                input, output = cap[:ind], cap[ind]
+                input = tf.keras.preprocessing.sequence.pad_sequences([input], maxlen=max_len, padding='post')[0] # taken from source code
+                output = tf.one_hot(output, depth=vocab_size)
+                X.append(input)
+                Y.append(output)
+
+    for image in img_to_captions:
+        if image in train_list:
+            split_helper(X_train, Y_train)
+        elif image in val_list:
+            split_helper(X_val, Y_val)
+        elif image in test_list:
+            split_helper(X_test, Y_test)
+        else:
+            ValueError("Image must be in one of the lists")
+
+    assert(len(X_train) == len(Y_train))
+    assert(len(X_val) == len(Y_val))
+    assert(len(X_test) == len(Y_test))
+
+    return np.array(X_train), np.array(Y_train), np.array(X_val), np.array(Y_val), np.array(X_test), np.array(Y_test)
 
     
 
@@ -58,13 +102,13 @@ if __name__ == "__main__":
 
     # TODO: construct vocab & lemmatize captions for only those images who are in train, val or test (exclude the 91 not used)
     data_file = os.path.join(ROOT, "data/Flickr8k_text", "Flickr8k.arabic.full.txt")
-    prep_data(data_file, all_imgs)
-    exit()
-    vocab, tokenized_captions = get_data(data_file)
-    # exit()
+    prepped_data_file = prep_data(data_file, all_imgs)
+    vocab, tokenized_captions = get_data(prepped_data_file)
 
-    print(f'Vocab size: {len(vocab)}') # TODO: vocab size is only 10,700 - isn't that small considering there are 8091 images?
-    print(f'Number of images: {len(tokenized_captions)}')
+    split_data(tokenized_captions, train_imgs, val_imgs, test_imgs, len(vocab))
+    
+    # print(f'Vocab size: {len(vocab)}') # TODO: vocab size is only 10,700 - isn't that small considering there are 8091 images?
+    # print(f'Number of images: {len(tokenized_captions)}')
 
     # initialize model
     model = EndtoEnd(arabic_vocab_size=len(vocab))
