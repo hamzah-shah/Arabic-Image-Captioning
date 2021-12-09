ff9a265fdec9318b444e87340e09b1313b2b08e6
Hamzah Shah <hamzahrshah@gmail.com>
2021-12-08 21:19:50 -0500

    created predict and test functions

--- code/main.py
+++ code/main.py
@@ -12,46 +12,39 @@ VGG_16FEATURES_FILE = os.path.join(ROOT, 'data/vgg16_features.pickle')
 DATA_FILE = os.path.join(ROOT, "data/Flickr8k_text", "Flickr8k.arabic.full.txt")
 TEST_IMGS_FILE = os.path.join(ROOT, "data/Flickr8k_text", "Flickr_8k.testImages.txt")
 
+
+START_TOKEN = "<START>"
+END_TOKEN = "<END>"
+SPACE = " "
+MAXLEN = 20
+
 def get_image_list(file):
     with open(file) as f:
         imgs = f.read().splitlines()
     assert(len(imgs) == len(set(imgs))) # no image listed more than once
     return imgs
 
-def split_data(img_to_caps, img_to_feats, train_list, test_list):
+def prep_data(img_to_caps, img_to_feats, train_list):
     '''
-    Prepares the data to be passed into the model.
+    Prepares the training data to be passed into the model.
     :param img_to_caps: dictionary mapping each image to a list of captions
-    :param img_to_feats: dictionary mapping each image to a 4096-dim feature vector
+    :param img_to_feats: dictionary mapping each image to a feature vector
     :param train_list: list of images in training set 
-    :param test_list: list of images in test set
-    :returns I_train, X_train, Y_train, I_test, X_test, Y_test
+    :returns I_train, X_train, Y_train
     '''
-    def split_helper(img, I, X, Y):
-        features, captions = img_to_feats[img], img_to_caps[img]
-
-        for cap in captions:
-            I.append(features)
-            input, label = np.delete(cap, -1), np.delete(cap, 0)
-            X.append(input)
-            Y.append(label)
-
     I_train, X_train, Y_train = [], [], []
-    I_test, X_test, Y_test = [], [], []
 
-    for image in img_to_caps:
-        if image in train_list:
-            split_helper(image, I_train, X_train, Y_train)
-        elif image in test_list:
-            split_helper(image, I_test, X_test, Y_test)
-        else:
-            raise Exception("Image must either be in the training data or testing data.")
+    for image in train_list:
+        features, captions = img_to_feats[image], img_to_caps[image]
+        for cap in captions:
+            I_train.append(features)
+            input, label = np.delete(cap, -1), np.delete(cap, 0)
+            X_train.append(input)
+            Y_train.append(label)
 
     assert(len(I_train) == len(X_train) == len(Y_train))
-    assert(len(I_test) == len(X_test) == len(Y_test))
 
-    return tf.squeeze(tf.convert_to_tensor(I_train)), tf.convert_to_tensor(X_train), tf.convert_to_tensor(Y_train), \
-        tf.squeeze(tf.convert_to_tensor(I_test)), tf.convert_to_tensor(X_test), tf.convert_to_tensor(Y_test)
+    return tf.squeeze(tf.convert_to_tensor(I_train)), tf.convert_to_tensor(X_train), tf.convert_to_tensor(Y_train)
 
 
 def get_features(image_list):
@@ -77,42 +70,56 @@ def get_features(image_list):
 
     
 
-def train(model_class, image_inputs, text_inputs, text_labels):
+def train(model_class, model, image_inputs, text_inputs, text_labels):
     '''
     Trains the decoder model.
     :param model_class: class for the decoder model
+    :param model
     :param image_inputs
     :param text_inputs
     :param text_labels
     :param pad_index
     '''
+    model.compile(optimizer=model_class.optimizer, loss=model_class.loss)
+    callbacks = [BleuCallback()]
+    model.fit(x=[image_inputs, text_inputs], y=text_labels, batch_size=model_class.batch_size, epochs=5, validation_split=0.2, callbacks=callbacks)
+
+
+
+def test(model, img_to_feats, testing_images):
+    '''
+    Tests the decoder model using greedy search.
+    :param model
+    :param img_to_feats: map from image to feature vector
+    :param testing_images: list of test images
+    '''
+    img2prediction = {}
+    for img in testing_images:
+        feat_vector = img_to_feats[img]
+        predicted_caption = predict_caption(model, feat_vector)
+        img2prediction[img] = predicted_caption
     
-    decoder = model_class.get_model()
-    decoder.compile(optimizer=model_class.optimizer, loss=model_class.loss)
-    callbacks = []
-    decoder.fit(x=[image_inputs, text_inputs], y=text_labels, batch_size=model_class.batch_size, epochs=5, validation_split=0.2, callbacks=callbacks)
-
-    # TODO: add a callback in decoder.fit to print out BlEU score
+    # TODO: BLEU stuff
 
 
+def predict_caption(model, image_feats):
+    '''
+    Given a trained model and an encoded image features, returns a predicted caption.
+    :param model
+    :param image_feats: an feature vector that encodes an image
+    return predicted caption
+    '''
+    cap = START_TOKEN
+    for _ in MAXLEN:
+        padded_cap = tf.keras.preprocessing.sequence.pad_sequences([cap], maxlen=MAXLEN, padding='post')
+        next_word_dist = model.predict(x=[image_feats, padded_cap])
+        next_word = tf.argmax(next_word_dist)
+        cap = cap + SPACE + next_word
 
-def test():
-    # TODO: predict captions using Itest, Xtest, Ytest
-    # TODO: report BLEU score and loss
-
-    # TODO: refer to the evaluate function in  this tutorial: https://www.tensorflow.org/tutorials/text/image_captioning 
-    # (i pulled this tutorial into Google Collab, which made it easy to understand and follow)
-        # also can look at the original src code - what we want to do is pretty similar
-        # both the link above and the src code do something called 'greedy search', which is to pick the next most likely word at every time step
-            # feed in the first word -> predict the second word
-                # feed in first & second -> predict third
-                    # feed in first, second, third -> predict fourth
-                        # keep going until predict <END>
-
-    # TODO: implement beam search, which is more optimal than greedy search
-
-    # TODO: 
-    pass
+        if next_word == END_TOKEN:
+            break
+    
+    return cap
 
 
 
@@ -125,22 +132,20 @@ if __name__ == "__main__":
     vocab, img2caps = get_data(DATA_FILE) # word2index, padded and tokenized caps
     img2features = get_features(all_imgs)
     
-    Itrain, Xtrain, Ytrain, Itest, Xtest, Ytest = split_data(img2caps, img2features, train_imgs, test_imgs)
+    Itrain, Xtrain, Ytrain = prep_data(img2caps, img2features, train_imgs)
 
-    print(f'Training on {len(Xtrain)} examples.')
-    print(f'Testing on {len(Xtest)} examples.')
+    print(f'Training on {len(Xtrain)} examples...')
+    print(f'Training on {len(train_imgs)} examples...')
+    print(f'Testing on {len(test_imgs)} images.')
 
-    print(f'ITrain {Itrain.shape} ')
-    print(f'XTrain {Xtrain.shape}')
-    print(f'YTrain {Ytrain.shape}')
-
-    print(f'Itest {Itest.shape} ')
-    print(f'Xtest {Xtest.shape}')
-    print(f'Ytest {Ytest.shape}')
+    # print(f'ITrain {Itrain.shape} ')
+    # print(f'XTrain {Xtrain.shape}')
+    # print(f'YTrain {Ytrain.shape}')
     
-
-    train(Decoder(vocab_size=len(vocab)), Itrain, Xtrain, Ytrain)
-    # test() # TODO: implement test, but don't call it
+    decoder_instance = Decoder(vocab_size=len(vocab))
+    decoder = decoder_instance.get_model()
+    train(decoder_instance, decoder, Itrain, Xtrain, Ytrain)
+    test(decoder, img2features, test_imgs)
 
     # TODO: make a function to plot loss and BLEU for both training and testing
     
